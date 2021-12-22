9f044896b835d90e3e1877e99f5c0ba6013b3d81
zigguy <96212654+zigguy@users.noreply.github.com>
2021-12-22 15:33:47 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,73 @@
+from tkinter import *
+from tkinter import messagebox
+import tkinter
 import random
-text_file = open("password.txt", "w+")
-linelist = []
-for line in text_file.readlines():
-    if line != "\n":
-        linelist.append(line)
-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
 
-while True:
-    password = ""
-    length = int(input("How long do you want your password to be? "))
-    for i in range(length):
-        password = password + random.choice(characters)
-    print(password)
-    print("Enjoy your password!")
-    log = input("Do you want to log this password? (y/n): ")
-    if log == "y":
-        linelist.append(password)
-    while log != "y" and log != "n":
-        print("Please enter y or n.")
-        log = input("Do you want to log this password? (y/n): ")
-        if log == "y":
-            linelist.append(password)    
-    play_again = input("Want to create another password? (y/n): ")
-    if play_again.lower() == "n":
-        text_file.close()
-        break
-    while play_again.lower() != "y" and play_again.lower() != "n":
-        print("Please enter y or n.")
-        play_again = input("Want to create another password? (y/n): ")
-        if play_again.lower() == "n":
-            text_file.close()
-            break
+#window
+window = Tk()
+window.geometry("300x300")
+window.title("Password Generator")
 
-updated_file = open("password.txt", "w")
-for line in linelist:
-    line = line.strip()
-    updated_file.write(line+"\n")
-updated_file.close()
+#lengthlabel
+length = IntVar()
+lengthlabel = Label(window, text="Password Length: ")
+lengthlabel.config(anchor=CENTER)
+lengthlabel.pack()
+lengthinput = Entry(window, textvariable=length)
+lengthinput.pack()
+
+#amountlabel
+amount = IntVar()
+amountlabel = Label(window,text = "Amount of Passwords")
+amountlabel.config(anchor=CENTER)
+amountlabel.pack()
+amountinput = Entry(window, textvariable=amount)
+amountinput.pack()
+
+#logcheckbox
+checkboxinput = IntVar()
+checkbox = Checkbutton(window, text="Do you want to log this password?", variable = checkboxinput, onvalue=1, offvalue=0)
+checkbox.pack()
+
+#adding a call function on click
+def showMsg():
+    text_file = open("password.txt", "r")
+    passwordlist = []
+    for line in text_file.readlines():
+        if line != "\n":
+            line = line.strip()
+            passwordlist.append(line)
+    text_file.close()
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
+    for i in range(amount.get()):
+        password = ""
+        for j in range(length.get()):
+            password = password + random.choice(characters)
+        if checkboxinput.get() == 1:
+            passwordlist.append(password)
+    boxstring = 'Your passwords have been generated!\n'
+    for i in range(len(passwordlist)):
+        boxstring = boxstring + passwordlist[i-1]+"\n"
+    messagebox.showinfo('Password', boxstring)
+    if len(passwordlist)!= 0:
+        updated_file = open("password.txt", "w")
+        for line in passwordlist:
+            line = line.strip()
+            updated_file.write(line+"\n")
+    else:
+        updated_file = open("password.txt", "r")
+    updated_file.close()
+
+#create button
+button = Button(window,
+                text='Submit',
+                width=10,
+                height=1,
+                command = showMsg)
+button.pack()
+
+
+
+#felt quirky
+
+window.mainloop()
